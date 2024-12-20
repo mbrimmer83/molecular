2732adbace5175cf464f0c514c2db2e0a78a9dda
Matthew Brimmer <mbrimmer1@gmail.com>
2024-12-20 09:50:37 -0500

    Update readme and home page

--- apps/documentation/app/routes/_index.tsx
+++ apps/documentation/app/routes/_index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div>
       <h1 className="text-2xl font-bold">Welcome to Molecular</h1>
-      <p>Your lightweight and composable state management library.</p>
+      <p>Your lightweight and composable state management library for React.</p>
     </div>
   )
 }
